e044262809130ca2dd2a0f339e17d773e44aace1
Roman Snegirev <rsng@mail.ru>
2021-11-23 15:53:48 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,11 +11,13 @@ except ImportError:
 
 version = None
 
-with codecs.open(os.path.join(os.path.abspath(os.path.dirname(
-        __file__)), 'python_socks', '_version.py'), 'r', 'latin1') as fp:
+with codecs.open(
+    os.path.join(os.path.abspath(os.path.dirname(__file__)), 'python_socks', '_version.py'),
+    'r',
+    'latin1',
+) as fp:
     try:
-        version = re.findall(r"^__version__ = '([^']+)'\r?$",
-                             fp.read(), re.M)[0]
+        version = re.findall(r"^__version__ = '([^']+)'\r?$", fp.read(), re.M)[0]
     except IndexError:
         raise RuntimeError('Unable to determine version.')
 
@@ -32,14 +34,14 @@ setup(
     version=version,
     license='Apache 2',
     url='https://github.com/romis2012/python-socks',
-    description=('Core proxy (SOCKS4, SOCKS5, HTTP tunneling) '
-                 'functionality for Python'),
+    description='Core proxy (SOCKS4, SOCKS5, HTTP tunneling) functionality for Python',
     long_description=long_description,
     long_description_content_type='text/markdown',
     packages=[
         'python_socks',
         'python_socks._proto',
         'python_socks.sync',
+        'python_socks.sync.v2',
         'python_socks.async_',
         'python_socks.async_.asyncio',
         'python_socks.async_.asyncio.v2',
@@ -54,5 +56,5 @@ setup(
         'trio': ['trio>=0.16.0'],
         'curio': ['curio>=1.4'],
         'anyio': ['anyio>=3.3.4'],
-    }
+    },
 )
